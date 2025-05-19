db1ec684d281bc8f8a4319ba4f3b37715af86d2c
Даниил <deadxraver@yandex.ru>
2025-05-19 15:14:47 +0300

    linear approximation done

--- approximators/linear.py
+++ approximators/linear.py
@@ -1,4 +1,18 @@
 def approximate(f, x_start, x_finish, h):
 	x = x_start
+	if x_start > x_finish or h < 0:
+		return None
+	sx, sxx, sy, sxy, n = 0, 0, 0, 0, 0
+	print(f'|\t№\t|\tx\t\t|\ty\t\t|')
 	while x < x_finish:
 		y = f(x)
+		n += 1
+		sx += x
+		sxx += x**2
+		sxy += x * y
+		sy += y
+		print(f'|\t{n}\t|\t{x:.3f}\t|\t{y:.3f}\t|')
+		x += h
+	a = (sxy * n - sx * sy) / (sxx * n - sx**2)
+	b = (sxx * sy - sx * sxy) / (sxx * n - sx ** 2)
+	return a, b
